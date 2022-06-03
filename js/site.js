f9fe4d92fdf76d56ebe9750a9d9b12c55b95b4bd
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-06-03 06:38:07 +0100

    Add Views and logic functions

--- js/site.js
+++ js/site.js
@@ -1,15 +1,38 @@
-// Call Hello World
-function HelloWorld() {
-    alert("Hello World!");
-}
-
 // CONTROLLER FUNCTION(S)
+// Get UI values
+function getValues() {
+    let loanValue = document.getElementById("loanValue").value;
+    let monthsValue = document.getElementById("monthsValue").value;
+    let rateValue = document.getElementById("rateValue").value;
 
+    // convert to Numbers
+    loanValue = Number(loanValue);
+    monthsValue = Number(monthsValue);
+    rateValue = Number(rateValue);
+
+
+}
 
 
 
 // LOGIC FUNCTION(S)
 
+
+function calcPayment(amount, rate, term) {
+    let monthlyRate = calcMonthlyRate(rate);
+    
+    let payment = (amount * rate) / (1 - Math.pow(1 + rate, -term));
+
+    return payment;
+}
+
+function calcMonthlyRate(rate){
+    return rate/1200;
+}
+
+function calcMonthlyInterest(balance, monthlyRate) {
+    return balance * monthlyRate;
+}
 
 
 
